992f72eb841ad6d7fe1c7f6e22a33cfb1d79bbe2
Василий Карабцов <karabcovas@mail.ru>
2023-11-19 03:40:21 +0300

    набросок

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,17 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'DackZi26@yandex.ru'
+EMAIL_HOST_PASSWORD = 'klavtwsqwgubvtkw'
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
--- users/views.py
+++ users/views.py
@@ -1,10 +1,34 @@
+from django.conf import settings
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.core.mail import send_mail
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from users.forms import UserForm
+from users.models import User
 
 
 class LoginView(BaseLoginView):
-    pass
+    template_name = 'users/login.html'
 
 
 class LogoutView(BaseLogoutView):
     pass
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/register.html'
+
+    def form_valid(self, form):
+        new_user = form.save()
+        send_mail(
+            subject='Поздравляю с регистрацией',
+            message='Вы зарегистрировались на нашей платформе, добро пожаловать!',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[new_user.email]
+        )
+        return super().form_valid(form)
